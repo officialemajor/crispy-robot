b07fcf7ba240aa05fe5d7bbdf137eee9b29cbd6f
Adebayo Emmanuel Ayomide <adebayoemmanuel2004@gmail.com>
2026-02-18 14:55:48 +0100

    Add script.js with dynamic features

---
+++ script.js
@@ -0,0 +1,6 @@
+// Current Date and Time in UTC
+const currentDateTime = new Date().toISOString().slice(0, 19).replace('T', ' ');
+
+console.log(`Current Date and Time (UTC): ${currentDateTime}`);
+
+// You can add more dynamic features here
